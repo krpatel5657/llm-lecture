50719c23aae2ebf5e91dee531a5f8c286db66331
Krishna Patel <krpatel5657@gmail.com>
2023-04-06 16:24:20 -0400

    WIP tic tac toe

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   const [player, setPlayer] = useState('X');
   const [winner, setWinner] = useState(null);
 
-  function handleClick(index) {
+  const handleClick = (index) => {
     if (winner || board[index]) return;
     const newBoard = [...board];
     newBoard[index] = player;
     setBoard(newBoard);
     setPlayer(player === 'X' ? 'O' : 'X');
-    setWinner(calculateWinner(newBoard));
-  }
+    checkForWinner(newBoard);
+  };
 
-  function calculateWinner(board) {
-    const lines = [
+  const checkForWinner = (board) => {
+    const winConditions = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -26,48 +26,44 @@ function App() {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return board[a];
+
+    for (let condition of winConditions) {
+      const [a, b, c] = condition;
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        setWinner(board[a]);
+        return;
       }
     }
-    if (board.every((cell) => cell !== null)) {
-      return 'draw';
-    }
-    return null;
-  }
 
-  function handleReset() {
+    if (!board.includes(null)) {
+      setWinner('draw');
+    }
+  };
+
+  const resetGame = () => {
     setBoard(Array(9).fill(null));
     setPlayer('X');
     setWinner(null);
-  }
+  };
 
   return (
-    <div className="App">
-      <h1>Tic Tac Toe</h1>
+    <div className="app">
+      <h1 className="title">Tic Tac Toe</h1>
+      {winner ? (
+        <h2 className="winner">{winner === 'draw' ? 'Draw!' : `${winner} wins!`}</h2>
+      ) : (
+        <h2 className="player">{`Player ${player}'s turn`}</h2>
+      )}
       <div className="board">
         {board.map((cell, index) => (
-          <div className={`cell ${cell}`} key={index} onClick={() => handleClick(index)}>
+          <div key={index} className={`cell ${cell}`} onClick={() => handleClick(index)}>
             {cell}
           </div>
         ))}
       </div>
-      <div className="status">
-        {winner ? (
-          winner === 'draw' ? (
-            <div>It's a draw!</div>
-          ) : (
-            <div>
-              {winner} wins!
-              <button onClick={handleReset}>Play again</button>
-            </div>
-          )
-        ) : (
-          <div>Current player: {player}</div>
-        )}
-      </div>
+      <button className="reset-button" onClick={resetGame}>
+        Reset Game
+      </button>
     </div>
   );
 }
